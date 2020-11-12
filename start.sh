bdae8c41ae5c34470270d06c8c3a713d5b3bd737
Kaj Oskar Rusilowski <xedom08@gmail.com>
2020-11-12 18:25:09 +0100

    test

--- start.sh
+++ start.sh
@@ -2,5 +2,5 @@
 
 WORKDIR = /home/game/lifearma/
 
-cd $WORKDIR/
+cd ${$WORKDIR}/
 ./arma3server -port=2302 "-config=config.cfg" "-cfg=basic.cfg" "-bepath=/opt/life/battleye" "-name=default" -filePatching -autoInit "-serverMod=@life_server;@extdb3"
