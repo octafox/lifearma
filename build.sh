17556d271db190f28804297663e147acc444c1e8
Kaj Oskar Rusilowski <xedom08@gmail.com>
2020-11-14 23:23:36 +0100

    fix build.sh

--- build.sh
+++ build.sh
@@ -2,19 +2,19 @@
 
 WORKDIR = /home/game/lifearma/
 
-cd ${WORKDIR}/
+cd $WORKDIR/
 git pull origin master
 
-cd ${WORKDIR}/@life_server/addons/
+cd $WORKDIR/@life_server/addons/
 rm -rf life_server.pbo
 armake pack life_server life_server.pbo
 
-cd ${WORKDIR}/@life_hc/addons/
+cd $WORKDIR/@life_hc/addons/
 rm -rf life_hc.pbo
 armake pack life_hc life_hc.pbo
 rm life_hc.pbo.life.bisign
-armake sign ${WORKDIR}/signKey/life.biprivatekey life_hc.pbo
+armake sign $WORKDIR/signKey/life.biprivatekey life_hc.pbo
 
-cd ${WORKDIR}/mpmissions/
+cd $WORKDIR/mpmissions/
 rm -rf life.altis.pbo
 armake pack life.altis life.altis.pbo
