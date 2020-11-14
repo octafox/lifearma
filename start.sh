a8fdfe470d3ee31a2296cfd1db1f12bce8274625
Kaj Oskar Rusilowski <xedom08@gmail.com>
2020-11-14 04:53:30 +0100

    fix start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-workDir = "/home/arma/server/"
+WORKDIR=/home/arma/server
 
-cd ${workDir}/
-./arma3server -port=2302 "-config=config.cfg" "-cfg=basic.cfg" "-bepath=${workDir}/battleye" "-profiles=${workDir}/profiles" "-name=lifearma" -filePatching -autoInit "-serverMod=@life_server;@extdb3"
+cd $WORKDIR
+./arma3server -port=2302 "-config=config.cfg" "-cfg=basic.cfg" "-bepath=${WORKDIR}/battleye" "-profiles=${WORKDIR}/profiles" "-name=lifearma" -filePatching -autoInit "-serverMod=@life_server;@extdb3"
