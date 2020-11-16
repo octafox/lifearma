f8933a62e4c11569e26d3f0c7e45cd3579545a74
Kaj Oskar Rusilowski <xedom08@gmail.com>
2020-11-16 13:40:07 +0100

    get faction members script

---
+++ bot/getFactionMembers.py
@@ -0,0 +1,37 @@
+import mariadb
+import re
+import sys
+import config
+
+try:
+    conn = mariadb.connect (user=config.DB_USER, password=config.DB_PASS, host=config.DB_HOST, port=config.DB_PORT, database=config.DB_NAME)
+except mariadb.Error as err:
+    print("Connection error")
+
+armalife = conn.cursor()
+if len(sys.argv)<2:
+    print("Select: ctrg - cop - med - reb")
+    
+else: 
+    if (sys.argv[1] == "ctrg"): 
+        armalife.execute("SELECT pid,name,coplevel FROM players WHERE coplevel='5' OR coplevel='6' OR coplevel='7'")
+        for pid,name,coplevel in armalife:
+            print(coplevel,name,f"[{pid}]")
+
+    elif (sys.argv[1] == "cop"): 
+        armalife.execute("SELECT pid,name,coplevel FROM players WHERE coplevel!='0' AND coplevel!='5' AND coplevel!='6' AND coplevel!='7'")
+        for pid,name,coplevel in armalife:
+            print(coplevel,name,f"[{pid}]")
+
+    elif (sys.argv[1] == "med"): 
+        armalife.execute("SELECT pid,name,mediclevel FROM players WHERE mediclevel!='0'")
+        for pid,name,mediclevel in armalife:
+            print(mediclevel,name,f"[{pid}]")
+
+    elif (sys.argv[1] == "reb"): 
+        armalife.execute("SELECT pid,name FROM players WHERE civ_licenses LIKE '%[`license_civ_rebel`,1]%'")
+        for pid,name in armalife:
+            print(name,f"[{pid}]")
+
+    else: print("Select: ctrg - cop - med - reb")
+
