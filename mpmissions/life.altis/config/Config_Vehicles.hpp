cc1f55da73e422220fe6b297e7b9c651ed93c0e4
Kaj Oskar Rusilowski <xedom08@gmail.com>
2020-11-19 22:33:30 +0100

    skin medici

--- mpmissions/life.altis/config/Config_Vehicles.hpp
+++ mpmissions/life.altis/config/Config_Vehicles.hpp
@@ -880,7 +880,7 @@ class LifeCfgVehicles {
                 "\a3\air_f\Heli_Light_02\Data\heli_light_02_ext_opfor_co.paa"
             }, "" },
             { "EMS White", "med", {
-                "#(argb,8,8,3)color(1,1,1,0.8)"
+                "textures\med\med_orc.jpg"
             }, "" }
         };
     };
