79c0d0208303e842c10ea9b512507933753f3963
Kaj Oskar Rusilowski <xedom08@gmail.com>
2020-11-21 04:07:05 +0100

    update restart.sh

--- restart.sh
+++ restart.sh
@@ -8,30 +8,6 @@ else
 fi
 
 cd $WORKDIR
-
-mkdir -p logs
-
-tmux kill-session -t arma
-tmux kill-session -t armaHC
-
-echo "> Server Started"
-tmux new -d -s arma './build.sh && ./start.sh &> ./logs/serverLogs_$(date "+%y%m%d%H%M%S").rpt'
-
-sleep 60
-
-echo "> HeadlessClient Started"
-tmux new -d -s armaHC './startHC.sh &> ./logs/hcLogs_$(date "+%y%m%d%H%M%S").rpt'
-
-#!/bin/bash
-
-if [ $# -eq 0 ]
-then
-    WORKDIR=/home/arma/server
-else
-    WORKDIR=$1
-fi
-
-cd $WORKDIR
 mkdir -p logs
 
 tmux kill-session -t arma
