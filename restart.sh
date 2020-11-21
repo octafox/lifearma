ae9f24e1ac0d8c635c1299cb094291fc01edf804
Kaj Oskar Rusilowski <xedom08@gmail.com>
2020-11-21 03:08:57 +0100

    update restart.sh

--- restart.sh
+++ restart.sh
@@ -17,7 +17,7 @@ tmux kill-session -t armaHC
 echo "> Server Started"
 tmux new -d -s arma './build.sh && ./start.sh &> ./logs/serverLogs_$(date "+%y%m%d%H%M%S").rpt'
 
-sleep 30
+sleep 60
 
 echo "> HeadlessClient Started"
 tmux new -d -s armaHC './startHC.sh &> ./logs/hcLogs_$(date "+%y%m%d%H%M%S").rpt'
