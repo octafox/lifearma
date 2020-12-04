7a5fbafa476cb5df143f8f5adf9dcc4d418de670
Kaj Oskar Rusilowski <xedom08@gmail.com>
2020-12-04 18:08:18 +0100

    cambio di formattazione

--- bot/getFactionMembers.py
+++ bot/getFactionMembers.py
@@ -16,22 +16,22 @@ else:
     if (sys.argv[1] == "ctrg"): 
         armalife.execute("SELECT pid,name,coplevel FROM players WHERE coplevel='5' OR coplevel='6' OR coplevel='7'")
         for pid,name,coplevel in armalife:
-            print(coplevel,name,f"[{pid}]")
+            print(f"{{{pid}}} {coplevel} {name}")
 
     elif (sys.argv[1] == "cop"): 
         armalife.execute("SELECT pid,name,coplevel FROM players WHERE coplevel!='0' AND coplevel!='5' AND coplevel!='6' AND coplevel!='7'")
         for pid,name,coplevel in armalife:
-            print(coplevel,name,f"[{pid}]")
+            print(f"{{{pid}}} {coplevel} {name}")
 
     elif (sys.argv[1] == "med"): 
         armalife.execute("SELECT pid,name,mediclevel FROM players WHERE mediclevel!='0'")
         for pid,name,mediclevel in armalife:
-            print(mediclevel,name,f"[{pid}]")
+            print(f"{{{pid}}} {mediclevel} {name}")
 
     elif (sys.argv[1] == "reb"): 
         armalife.execute("SELECT pid,name FROM players WHERE civ_licenses LIKE '%[`license_civ_rebel`,1]%'")
         for pid,name in armalife:
-            print(name,f"[{pid}]")
+            print(f"{{{pid}}} {name}")
 
     else: print("Select: ctrg - cop - med - reb")
 
