0d1261acbc93104e0adad6c2a0e283705631d2dd
simone mastella <35423599+simonemastella@users.noreply.github.com>
2020-12-06 13:45:12 +0100

    fix response di flask

--- bot/flaskTest.py
+++ bot/flaskTest.py
@@ -5,24 +5,23 @@ import dbLifeFnc as life
 app = flask.Flask(__name__)
 app.config["DEBUG"] = True
 
-# Create some test data for our catalog in the form of a list of dictionaries.
 @app.route('/api/player', methods=['GET'])
 def api_sid():
-    # Check if an ID was provided as part of the URL.
-    # If ID is provided, assign it to a variable.
-    # If no ID is provided, display an error in the browser.
     if 'sid' in request.args:
         sid = (request.args['sid'])
-
     else:
-        return "Error: No id field provided. Please specify an id."
+        return "Error: No id field provided. Please specify an sid."
 
-    
-
-    return jsonify(life.apiGetPlayerBySID(sid))
+    response = app.response_class(response=life.apiGetPlayerBySID(sid),
+                                  status=200,
+                                  mimetype='application/json')
+    return response
 
 @app.route('/api/allPlayers', methods=['GET'])
 def api_all():
-    return jsonify(life.apiGetAllPlayers())
+    response = app.response_class(response=life.apiGetAllPlayers(),
+                                  status=200,
+                                  mimetype='application/json')
+    return response
 
 app.run()
